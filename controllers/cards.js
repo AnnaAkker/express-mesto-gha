f02914fd5080f975e5ed182a561cce08bbb20a9f
Anna <122319400+AnnaAkker@users.noreply.github.com>
2023-09-22 16:35:42 +0300

    test 9

--- controllers/cards.js
+++ controllers/cards.js
@@ -18,54 +18,54 @@ module.exports.addCard = (req, res, next) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then((card) => {
+      if (!card) {
+        return next(new NotFoundError('Карточка с таким _id не найдена.'));
+      }
+
       Card.findById(card._id)
-        .orFail()
         .populate('owner')
         .then((data) => res.status(HTTP_STATUS_CREATED).send(data))
         .catch((err) => {
           if (err instanceof mongoose.Error.DocumentNotFoundError) {
-            next(new NotFoundError('Карточка с таким _id не найдена.'));
-          } else {
-            next(err);
+            return next(new NotFoundError('Карточка с таким _id не найдена.'));
           }
+          return next(err);
         });
     })
     .catch((err) => {
       if (err instanceof mongoose.Error.ValidationError) {
-        next(new BadRequestError(err.message));
-      } else {
-        next(err);
+        return next(new BadRequestError(err.message));
       }
+      return next(err);
     });
 };
 
 module.exports.deleteCard = (req, res, next) => {
   Card.findById(req.params.cardId)
     .then((card) => {
+      if (!card) {
+        return next(new NotFoundError(`Карточка с таким _id: ${req.params.cardId} не найдена.`));
+      }
+
       if (!card.owner.equals(req.user._id)) {
         throw new ForbiddenError('Карточка другого пользователя');
       }
-      Card.deleteOne(card)
-        .orFail()
+
+      Card.deleteOne({ _id: card._id })
         .then(() => {
           res.status(HTTP_STATUS_OK).send({ message: 'Карточка удалена' });
         })
-        .catch((err) => {
-          if (err instanceof mongoose.Error.DocumentNotFoundError) {
-            next(new NotFoundError(`Карточка с таким _id: ${req.params.cardId} не найдена.`));
-          } else {
-            next(err);
-          }
-        });
+        .catch(next);
     })
     .catch(next);
 };
 
 module.exports.likeCard = (req, res, next) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
-    .orFail()
-    .populate(['owner', 'likes'])
     .then((card) => {
+      if (!card) {
+        return next(new NotFoundError(`Карточка с таким _id: ${req.params.cardId} не найдена.`));
+      }
       res.status(HTTP_STATUS_OK).send(card);
     })
     .catch(next);
@@ -73,9 +73,10 @@ module.exports.likeCard = (req, res, next) => {
 
 module.exports.dislikeCard = (req, res, next) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
-    .orFail()
-    .populate(['owner', 'likes'])
     .then((card) => {
+      if (!card) {
+        return next(new NotFoundError(`Карточка с таким _id: ${req.params.cardId} не найдена.`));
+      }
       res.status(HTTP_STATUS_OK).send(card);
     })
     .catch(next);
